1a03c42dfe44ab7b3157c7fba68c2e0afda2f6ec
Ben Zoon <ben.zoon@navexglobal.com>
2023-02-28 11:37:07 -0700

    tiny update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
-import { discoverEntries, exploredQuery } from './api/api';
+import { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import CreateScreen from './components/CreateScreen/CreateScreen';
+import SolveScreen from './components/SolveScreen/SolveScreen';
 import { setUserId } from './lib/utils';
 
 function App() {
@@ -9,9 +11,14 @@ function App() {
     }, []);
 
     return (
-        <>
-            <div />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<CreateScreen />}>
+                    <Route path="solve/:id" element={<SolveScreen />} />
+                    <Route path="*" element={<CreateScreen />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
